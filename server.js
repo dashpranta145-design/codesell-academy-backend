b1206d557cf15253f1ddd24547de1741d4ff025b
Sanjit Dash <dash.pranta145@gmail.com>
2025-11-13 09:36:01 +0600

    Server is updated

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [
-      "https://codesell-academy.vercel.app",
-      "http://localhost:5173", // for local development
-      "http://localhost:3000", // for local development
-    ],
+    origin: process.env.FRONTEND_URL || "*",
     credentials: true,
   })
 );
